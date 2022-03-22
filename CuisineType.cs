518c4ebdab4ed923f05e3ee4dcd43d780d29777a
Yassine Naji <shinobinaji@gmail.com>
2022-03-22 20:33:09 +0000

    modify cuisineType

--- CuisineType.cs
+++ CuisineType.cs
@@ -1,12 +1,18 @@
-﻿namespace OdeToFood.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Core
 {
     public partial class Restaurant
     {
         public enum CuisineType
         {
+            /*[Display(Name = "None")]*/
             none,
+            /*[Display(Name = "Italian")]*/
             Italian,
+            /*[Display(Name = "Indian")]*/
             Indian,
+           /* [Display(Name = "Moroccan")]*/
             Moroccan,
 
 
--- Restaurant.cs
+++ Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace OdeToFood.Core
 {
     public partial class Restaurant
     {
+
         public int Id { get; set; }
+        [Required,StringLength(80)]
         public string Name { get; set; }
+        [Required, StringLength(255)]
         public string Location { get; set; }
         public CuisineType Cuisine { get; set; }
 
